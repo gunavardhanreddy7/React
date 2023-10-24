13893526b750eababf970a9e5455da72d33ba4a7
GUNAVARDHAN JAKKIDI <vardhanreddy226@gmail.com>
2023-10-23 22:52:14 -0400

    use reducer files

--- src/EffectDesc.js
+++ src/EffectDesc.js
@@ -13,11 +13,19 @@ function reducer(state, action) {
       return state;
   }
 }
+function red(state, action) {
+  switch (action.type) {
+    case "ADD_TO_DO":
+      return [...state, action.payload.name];
+  }
+}
 
 function EffectDesc() {
   const [resourceType, setResourceType] = useState("posts");
   const [items, setItems] = useState([]);
   const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [todos, disp] = useReducer(red, []);
+  const [name, setName] = useState("");
 
   function incre() {
     dispatch({ type: "increment" });
@@ -25,14 +33,29 @@ function EffectDesc() {
   function decre() {
     dispatch({ type: "decrement" });
   }
+  function handleSubmit(e) {
+    e.preventDefault();
+    disp({ type: "ADD_TO_DO", payload: { name: name } });
+    setName("");
+  }
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
       .then((response) => response.json())
       .then((json) => setItems(json));
     console.log("Changed");
   }, [resourceType]);
+  console.log(todos);
   return (
     <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </form>
+      </div>
       <button onClick={() => setResourceType("posts")}>Posts</button>
       <button onClick={() => setResourceType("users")}>users</button>
       <button onClick={() => setResourceType("Comments")}>Comments</button>
